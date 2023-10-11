be9d6169c32779449a32258e44128877bf10d9bc
NewM07x2 <newmtube07@gmail.com>
2023-10-11 20:59:38 +0900

    add MyLibrary

---
+++ MyLibrary/src/constants/url.js
@@ -0,0 +1,12 @@
+export default {
+  "BASE_URL": "",
+  // 機能名: {
+  //   "GET_USER_INFO": "",
+  //   "CREATE_USER_INFO": "",
+  //   "INSERT_USER_INFO": "",
+  //   "UPDATE_USER_INFO": "",
+  //   "DELETE_USER_INFO": "",
+  //   "CHACK_USER_INFO": "",
+  //   "SEARCH_USER_INFO": ""
+  // }
+};
